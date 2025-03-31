01e4d7394e0cbe7c00915cbbdd4cae8d25943c1e
shashi-manikonda <manikonda@outlook.com>
2025-03-31 01:30:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='MTFLibrary',
-    version='0.1.0',
+    version='1.0.1',
     description='Multivariate Taylor Expansion Library',
     author='Shashikant Manikonda',
     author_email='manikonda@outlook.com',
