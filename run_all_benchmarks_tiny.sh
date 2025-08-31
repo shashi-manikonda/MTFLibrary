a294b1211966f94898147cc4683dde2d930aecb8
Shashikant Manikonda <manikonda@outlook.com>
2025-08-31 11:39:05 -0400

    cleanup

--- run_all_benchmarks_tiny.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "--- Tiny Size (10 terms, 50 points) ---"
-echo "Python:"
-python benchmarks/benchmark.py --implementation python --num-terms 10 --num-points 50
-echo "C:"
-python benchmarks/benchmark.py --implementation c --num-terms 10 --num-points 50
-echo "C++:"
-python benchmarks/benchmark.py --implementation cpp --num-terms 10 --num-points 50
